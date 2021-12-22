6c6a70759a65aa3af35b353f13494ebfc3e19326
nparashar150 <nparashar150@gmail.com>
2021-12-22 19:44:49 +0530

    Remove unused vars & comments

--- src/components/Shared/Navbar/Navbar.js
+++ src/components/Shared/Navbar/Navbar.js
@@ -116,11 +116,11 @@ const Navbar = () => {
     }
   };
 
-  const { dispatch, user, isFetching } = useContext(AuthContext);
+  const { dispatch, isFetching } = useContext(AuthContext);
   const handleLogin = async () => {
     signIn(dispatch);
   };
-  console.log(user);
+  // console.log(user);
   useEffect(() => {
     window.screen.width >= 768 ? setToggleNav(true) : setToggleNav(false);
   }, []);
