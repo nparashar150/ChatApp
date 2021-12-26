0e8b62ccf77f9ddd0a3dcc3f47b0fbd4ac31b97e
nparashar150 <nparashar150@gmail.com>
2021-12-27 00:47:08 +0530

    Create Submit Button Template

--- src/components/Shared/Button/Button.js
+++ src/components/Shared/Button/Button.js
@@ -25,3 +25,28 @@ export const Button = styled(Link)`
     margin: 1rem 0 0 0;
   }
 `;
+
+export const ButtonSubmit = styled.button`
+  padding: ${(props) => props.padding ? props.padding : ".75rem 2.25rem"};
+  border: none;
+  border-radius: ${(props) => props.radius ? props.radius : "1rem"};
+  background: ${red};
+  color: ${white};
+  font-family: "Nunito" sans-serif;
+  font-weight: 600;
+  font-size: ${(props) => props.fontSize ? props.fontSize : "1.25rem"};
+  /* margin: .25rem 0 0 .25rem; */
+  border: 2px solid ${red};
+  text-decoration: none;
+
+  &:hover,
+  &:focus {
+    color: ${darkBlue};
+    background: ${red + "35"};
+  }
+
+  @media (max-width: 768px) {
+    /* margin: 1rem 0 0 0; */
+    width: ${(props) => props.mediaWidth ? props.mediaWidth : ""};
+  }
+`;
