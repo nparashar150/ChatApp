0992babdfe9867c1081d8ef21c201e478ea661d0
nparashar150 <nparashar150@gmail.com>
2021-12-28 23:38:38 +0530

    Replace multer with cloud image storage

--- src/routes/imageRoute.js
+++ src/routes/imageRoute.js
@@ -1,33 +1,56 @@
 const router = require("express").Router();
-const imageInfo = require("../models/image.model");
-const upload = require("../middlewares/imageUpload");
+const {
+  uploadFile,
+  generatePublicUrl,
+} = require("../middlewares/googleDriveHandler");
+// const imageInfo = require("../models/image.model");
+// const upload = require("../middlewares/imageUpload");
 
-router.post("/", upload.single("image"), async (req, res) => {
+// router.post("/", upload.single("image"), async (req, res) => {
+//   try {
+//     if (!(req.body.email && req.file)) {
+//       res.send({ status: 400, message: "Incomplete Information" });
+//     }
+//     let imageData = new imageInfo({
+//       email: req.body.email,
+//     });
+//     if (req.file) {
+//       imageData.image = req.file.path;
+//     }
+//     const exists = false;
+//     if (exists) {
+//       res.send({ status: 400, message: "The image already exists" });
+//     } else {
+//       const saveImage = await imageData.save();
+//       res
+//         .status(200)
+//         .json({
+//           saveImage,
+//           url: `/.netlify/functions/server/user/image/upload/file/${saveImage.image.replace(
+//             "uploads/",
+//             ""
+//           )}`,
+//         });
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
+
+router.post("/getPhotoURL", async (req, res) => {
   try {
-    if (!(req.body.email && req.file)) {
-      res.send({ status: 400, message: "Incomplete Information" });
-    }
-    let imageData = new imageInfo({
-      email: req.body.email,
-    });
-    if (req.file) {
-      imageData.image = req.file.path;
-    }
-    const exists = false;
-    if (exists) {
-      res.send({ status: 400, message: "The image already exists" });
+    const { base64, mimeType, fileName } = req.body;
+    if (base64 && mimeType && fileName) {
+      let response = await uploadFile(base64, mimeType, fileName);
+      let publicURL = await generatePublicUrl(response.data.id);
+      res
+        .status(200)
+        .json({ uploadStatus: response.data, URL: publicURL.data });
     } else {
-      const saveImage = await imageData.save();
-      res.status(200).json({saveImage, url: `/.netlify/functions/server/user/image/upload/file/${saveImage.image.replace("uploads/", "")}`});
+      res.status(400).json({ message: "Incomplete Data" });
     }
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
+    res.status(500).json({ message: err });
   }
 });
-
-router.get("/:email", async (req, res) => {
-  const image = await imageInfo.find({ email: req.params.email });
-  res.json(image);
-});
-
 module.exports = router;
